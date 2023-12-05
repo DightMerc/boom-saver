3d2afa721444d20e2a436aa07f15f0e0042d9dac
d.ibragimov <d.ibragimov@qiwi.uz>
2023-12-05 18:36:02 +0500

    feat: proxies added

--- backends/async_backends/instagram.py
+++ backends/async_backends/instagram.py
@@ -1,9 +1,17 @@
+import json
 import os
+import random
+import time
 import uuid
-from typing import Dict, Callable
+from typing import Dict
+
+from redis import Redis
 
 import httpx
+import instaloader
+import orjson
 from instaloader import Instaloader, Post
+from requests_tor import RequestsTor
 
 from backends import AbstractBackendResult, AsyncAbstractBackend
 from backends.exceptions import ObjectNotFound, UnsupportedMediaType
@@ -15,14 +23,69 @@ class InstagramBackendResult(AbstractBackendResult):
         self.file = file
 
 
+class MyRateController(instaloader.RateController):
+    def sleep(self, secs: float):
+        time_to_sleep = random.randint(1, 3)
+        print(f"Time to sleep: {time_to_sleep}")
+        time.sleep(time_to_sleep)
+
+    def count_per_sliding_window(self, query_type):
+        return 20
+
+
 class AsyncInstagramBackend(AsyncAbstractBackend):
 
     BACKEND_URIS = ["instagram"]
 
     def __init__(self, link: str, path: str):
         self.link = link
-        self.backend: Instaloader = Instaloader()
+        self.redis = Redis(
+            host=os.environ["REDIS_HOST"],
+            port=int(os.environ["REDIS_PORT"]),
+            db=int(os.environ["REDIS_DB"]),
+        )
+        self.backend: Instaloader = Instaloader(
+            sleep=True,
+            rate_controller=lambda ctx: MyRateController(ctx),
+            download_geotags=False,
+            download_comments=False,
+            download_pictures=False,
+            download_video_thumbnails=False,
+            sanitize_paths=True,
+            proxies=self._get_proxies(),
+        )
         self.path = path
+
+    def _get_proxies(self) -> Dict:
+        proxies = self.redis.get("proxies")
+        if not proxies:
+            new_proxies = {}
+            for port in range(9050, 9062):
+                active = True
+                if port == 9050:
+                    active = False
+                new_proxies[f"{port}"] = dict(active=active)
+
+            self.redis.set("proxies", json.dumps(new_proxies))
+            return {"https": f"socks5://localhost:9050"}
+        else:
+            ports: Dict = json.loads(proxies.decode("ascii"))
+            for port in ports:
+                if ports[port]["active"]:
+                    ports[port]["active"] = False
+                    self.redis.set("proxies", json.dumps(ports))
+                    return {"https": f"socks5://localhost:{port}"}
+            requestor = RequestsTor(tor_ports=(9050,), tor_cport=9051)
+            requestor.new_id()
+            new_proxies = {}
+            for port in range(9050, 9062):
+                active = True
+                if port == 9050:
+                    active = False
+                new_proxies[f"{port}"] = dict(active=active)
+
+            self.redis.set("proxies", json.dumps(new_proxies))
+            return {"https": f"socks5://localhost:9050"}
 
     async def _get_file(self, post) -> Dict:
         if post.is_video:
