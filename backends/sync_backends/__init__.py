6c7798a88b75ac74e2ea183d115a9e169b786474
d.ibragimov <d.ibragimov@qiwi.uz>
2023-12-06 16:51:24 +0500

    fix: sync removed

--- backends/sync_backends/__init__.py
+++
@@ -1,9 +0,0 @@
-from backends import AbstractBackendResult
-
-
-class SyncSaverBackend:
-    def __init__(self, backend):
-        self.backend = backend
-
-    def get(self) -> AbstractBackendResult:
-        return self.backend.get()
